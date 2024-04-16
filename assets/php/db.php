acc70351e1aded855b30dd2a380a3eea23356203
Robotnik08 <89689979+Robotnik08@users.noreply.github.com>
2024-04-16 23:32:21 +0200

    projects database link

---
+++ assets/php/db.php
@@ -0,0 +1,22 @@
+<?php
+    $db_host = 'localhost';
+    $db_user = 'root';
+    $db_pass = '';
+
+    $db_name = 'portfolio';
+    $table_name = 'projects';
+
+    // PDO connection
+    try {
+        $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        die("Could not connect to the database $db_name :" . $e->getMessage());
+    }
+
+    // Fetching data
+    $stmt = $pdo->prepare("SELECT * FROM $table_name");
+    $stmt->execute();
+    $projects = $stmt->fetchAll();
+    $stmt = null;
+    $pdo = null;
--- assets/php/projects_block.php
+++ assets/php/projects_block.php
@@ -1,76 +1,27 @@
-<div class="even project-spot flex snap-start">
-                                <div class="text-block w-full">
-                                    <h2 class="text-7xl m-5">Project name</h2>
-                                    <p class="text-2xl sm:text-4xl m-5">Project description Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos eius nesciunt vero perferendis repellat sint, corporis architecto ducimus debitis rem, modi fugit pariatur aperiam? Saepe quos suscipit maxime molestiae sed?</p>
+<?php
+    // connect to database
+    require_once 'db.php';
 
-                                    <div class="buttons flex flex-row relative">
-                                        <button class="git-button bg-white mx-auto text-7xl w-[40%] h-full text-green-800 m-1">
-                                            Github
-                                        </button>
-                                        <button class="more-button bg-white mx-auto text-7xl w-[40%] h-full text-green-800 m-1">
-                                            More...
-                                        </button>
-                                    </div>
+    for ($i = 0; $i < count($projects); $i++) { ?>
+    <div class="<?php echo ($i % 2 == 0 ? 'even' : 'uneven'); ?> project-spot flex snap-start">
+        <div class="text-block w-full">
+            <h2 class="text-7xl m-5"><?php echo $projects[$i]['name']; ?></h2>
+            <p class="text-2xl sm:text-4xl m-5"><?php echo $projects[$i]['description']; ?></p>
 
-                                </div>
-                                <div class="image-block h-full aspect-square">
-                                    <img class="w-full h-full object-contain" src="assets/img/template.png" alt="Project image">
-                                </div>
-                            </div>
-                            <div class="uneven project-spot flex flex-row-reverse snap-start">
-                                <div class="text-block w-full">
-                                    <h2 class="text-7xl m-5">Project name</h2>
-                                    <p class="text-2xl sm:text-4xl m-5">Project description Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos eius nesciunt vero perferendis repellat sint, corporis architecto ducimus debitis rem, modi fugit pariatur aperiam? Saepe quos suscipit maxime molestiae sed?</p>
+            <div class="buttons flex flex-row relative">
+                <a href="<?php echo $projects[$i]['github_link']; ?>" class="git-button bg-white mx-auto text-7xl w-[40%] h-full text-green-800 m-1 text-center">
+                    Github
+                </a>
+                <?php if ($projects[$i]['demo_link'] != 'none') { ?>
+                <a href="<?php echo $projects[$i]['demo_link']; ?>"  class="demo-button bg-white mx-auto text-7xl w-[40%] h-full text-green-800 m-1 text-center">
+                    Demo
+                </a>
+                <?php } ?>
+            </div>
 
-                                    <div class="buttons flex flex-row relative">
-                                        <button class="git-button bg-white mx-auto text-7xl w-[40%] h-full text-green-800 m-1">
-                                            Github
-                                        </button>
-                                        <button class="more-button bg-white mx-auto text-7xl w-[40%] h-full text-green-800 m-1">
-                                            More...
-                                        </button>
-                                    </div>
-
-                                </div>
-                                <div class="image-block h-full aspect-square">
-                                    <img class="w-full h-full object-contain" src="assets/img/template.png" alt="Project image">
-                                </div>
-                            </div>
-                            <div class="even project-spot flex snap-start">
-                                <div class="text-block w-full">
-                                    <h2 class="text-7xl m-5">Project name</h2>
-                                    <p class="text-2xl sm:text-4xl m-5">Project description Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos eius nesciunt vero perferendis repellat sint, corporis architecto ducimus debitis rem, modi fugit pariatur aperiam? Saepe quos suscipit maxime molestiae sed?</p>
-
-                                    <div class="buttons flex flex-row relative">
-                                        <button class="git-button bg-white mx-auto text-7xl w-[40%] h-full text-green-800 m-1">
-                                            Github
-                                        </button>
-                                        <button class="more-button bg-white mx-auto text-7xl w-[40%] h-full text-green-800 m-1">
-                                            More...
-                                        </button>
-                                    </div>
-
-                                </div>
-                                <div class="image-block h-full aspect-square">
-                                    <img class="w-full h-full object-contain" src="assets/img/template.png" alt="Project image">
-                                </div>
-                            </div>
-                            <div class="uneven project-spot flex flex-row-reverse snap-start">
-                                <div class="text-block w-full">
-                                    <h2 class="text-7xl m-5">Project name</h2>
-                                    <p class="text-2xl sm:text-4xl m-5">Project description Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos eius nesciunt vero perferendis repellat sint, corporis architecto ducimus debitis rem, modi fugit pariatur aperiam? Saepe quos suscipit maxime molestiae sed?</p>
-
-                                    <div class="buttons flex flex-row relative">
-                                        <button class="git-button bg-white mx-auto text-7xl w-[40%] h-full text-green-800 m-1">
-                                            Github
-                                        </button>
-                                        <button class="more-button bg-white mx-auto text-7xl w-[40%] h-full text-green-800 m-1">
-                                            More...
-                                        </button>
-                                    </div>
-
-                                </div>
-                                <div class="image-block h-full aspect-square">
-                                    <img class="w-full h-full object-contain" src="assets/img/template.png" alt="Project image">
-                                </div>
-                            </div>
+        </div>
+        <div class="image-block aspect-square">
+            <img class="object-cover" src="assets/img/<?php echo $projects[$i]['image_link']; ?>" alt="Project image">
+        </div>
+    </div>
+<?php } ?>
